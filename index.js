1cc316d8351e81c7c9003922c7c87316cf97eae3
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-09 16:10:18 +0300

    commit tree efd997dd91748f555b0c9903db84f88b252b9136

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName='bob'
+const leastFavoriteCustomer= 'bobby'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName
+    
+}
+function setBestCustomer(){
+     bestCustomer= 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+    return bestCustomer
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'bobb'
+}
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -22,7 +22,7 @@ describe('Fix the Scope', function() {
 
       expect(window.bestCustomer).to.equal('not bob');
     });
-  });
+  });   
 
   describe('overwriteBestCustomer()', function() {
     it('overwrites the best customer', function() {
